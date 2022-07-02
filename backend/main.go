f5cbc4e0358abc349f85221c2bc1261cdd76d523
Starovoytov Alexandr <saniy200311@gmail.com>
2022-07-02 19:03:22 +0300

    Добавлены docker-среды для Go

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 type entry struct {
-	ID string `json:"id"`
+	ID    string `json:"id"`
 	Value string `json:"value"`
 }
 
-var entries = []entry {
+var entries = []entry{
 	{ID: "1", Value: "12345"},
 	{ID: "2", Value: "Vyacheslav, где фронт?"},
 	{ID: "351", Value: "pukpuk"},
@@ -20,11 +21,22 @@ var entries = []entry {
 func main() {
 	router := gin.Default()
 	router.GET("/entries", getEntries)
+	router.GET("/ping", ping)
 
-	router.Run("localhost:8080")
+	router.SetTrustedProxies(nil)
+
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router.Run("0.0.0.0:" + port)
 }
 
 func getEntries(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, entries)
 }
 
+func ping(c *gin.Context) {
+	c.String(http.StatusOK, `{"Status" : "OK"}`)
+}
